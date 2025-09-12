483546bcabd2fe9f799e00f331dea946674b3c72
kushagra singhal <kushagrasinghal987@gmail.com>
2025-09-12 12:13:55 +0530

    Doctor's data

---
+++ app/(routes)/dashboard/_components/HistoryList.tsx
@@ -0,0 +1,22 @@
+"use client";
+import { Button } from '@/components/ui/button';
+import Image from 'next/image';
+import React, { useState } from 'react'
+
+function HistoryList(){
+    const[historyList,setHistoryList]  =useState([]);
+  return (
+    <div className='mt-10'>
+        {historyList.length == 0?
+        <div className='flex items-center flex-col justify-between p-7 border border-dashed rounded-2xl border-2 '>
+            <Image src={'/medical-assistance.png'} alt="empty" width={150} height={150}></Image>
+            <h2 className='font-bold text-xl mt-2'>No recent Conversations</h2>
+            <p>It looks like you haven't consulted aith any doctors yet.</p>
+            <Button className='mt-5 '>+ Start a Consultation</Button>
+</div>
+:<div>List</div>
+}
+    </div>
+  )
+} 
+export default HistoryList
--- app/(routes)/dashboard/page.tsx
+++ app/(routes)/dashboard/page.tsx
@@ -1,8 +1,17 @@
 import React from 'react'
+import HistoryList from './_components/HistoryList'
+import { Button } from '@/components/ui/button'
 
 function Dashboard() {
   return (
-    <div>workspace</div>
+    <div>
+      <div className='flex justify-between items-center'>
+ <h2 font-bold text-2xl >My Dashboard</h2>  
+ <Button>+ Consult with Doctor</Button>
+      </div>
+     
+      <HistoryList/>
+      </div>
   )
 }
 
