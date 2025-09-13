a67e5feaa6fc7d300cb0f68d8fd51e3cf4101400
kushagra singhal <kushagrasinghal987@gmail.com>
2025-09-13 12:23:45 +0530

    dialog box

---
+++ app/(routes)/dashboard/_components/AddNewSessionDialog.tsx
@@ -0,0 +1,45 @@
+"use client"
+import React, { useState } from 'react'
+import {
+  Dialog,
+  DialogClose,
+  DialogContent,
+  DialogDescription,
+  DialogFooter,
+  DialogHeader,
+  DialogTitle,
+  DialogTrigger,
+} from "@/components/ui/dialog"
+import { Button } from '@/components/ui/button'
+import { Textarea } from '@/components/ui/textarea'
+import { ArrowBigRight } from 'lucide-react'
+function AddNewSessionDialog() {
+    const [note,setNote] = useState<string>();
+  return (
+   <Dialog>
+  <DialogTrigger>
+      <Button className='mt-3 '>+ Start a Consultation</Button>
+  </DialogTrigger>
+  <DialogContent>
+    <DialogHeader>
+      <DialogTitle>Add basic Details</DialogTitle>
+      <DialogDescription asChild>
+    <div>
+        <h2>Add symptoms or Any Other Details</h2>
+        <Textarea placeholder='ADD detail here..' className='h-[250px] mt-1'
+            onChange ={(e)=>setNote(e.target.value)} /> 
+    </div> 
+      </DialogDescription>
+    </DialogHeader>
+    <DialogFooter>
+        <DialogClose>
+ <Button variant ={'outline'}>Cancel</Button>
+        </DialogClose>
+       
+        <Button disabled={!note}>Next <ArrowBigRight/></Button>
+    </DialogFooter>
+  </DialogContent>
+</Dialog>
+  )
+}
+export default AddNewSessionDialog
--- app/(routes)/dashboard/_components/HistoryList.tsx
+++ app/(routes)/dashboard/_components/HistoryList.tsx
@@ -2,6 +2,7 @@
 import { Button } from '@/components/ui/button';
 import Image from 'next/image';
 import React, { useState } from 'react'
+import AddNewSessionDialog from './AddNewSessionDialog';
 
 function HistoryList(){
     const[historyList,setHistoryList]  =useState([]);
@@ -12,7 +13,7 @@ function HistoryList(){
             <Image src={'/medical-assistance.png'} alt="empty" width={150} height={150}></Image>
             <h2 className='font-bold text-xl mt-2'>No recent Conversations</h2>
             <p>It looks like you haven't consulted aith any doctors yet.</p>
-            <Button className='mt-5 '>+ Start a Consultation</Button>
+          <AddNewSessionDialog />
 </div>
 :<div>List</div>
 }
